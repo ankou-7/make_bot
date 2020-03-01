e1b9084648c4106057def1171ac4bc22dacc2a47
安江洸希 <yasuekouki@ankou.local>
2020-03-02 07:11:18 +0900

    add new file

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ from linebot.models import (
    FollowEvent, MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage, TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction, MessageTemplateAction, URITemplateAction,
        QuickReplyButton, MessageAction, QuickReply,
 )
-#from keras.models import load_model
-
+from keras.models import load_model
+import numpy as np
+import tensorflow as tf
 import os
 import wikipedia
 import patarn_match as pat
@@ -65,8 +66,39 @@ for i, char in enumerate(chars_list):
 n_char = len(chars_list)
 max_length_x = 128
 
-#encoder_model = load_model('make_monogatari/model/encoder_model.h5')
-#decoder_model = load_model('make_monogatari/model/decoder_model.h5')
+encoder_model = load_model('make_monogatari/model/encoder_model.h5')
+graph_en = tf.get_default_graph()
+
+decoder_model = load_model('make_monogatari/model/decoder_model.h5')
+graph_de = tf.get_default_graph()
+
+def respond(message, max_length_x, n_char, char_indices, indices_char, beta=5):
+    vec = mono.sentence_to_vector(message,max_length_x,n_char,char_indices)  # 文字列をone-hot表現に変換
+    global graph
+    with graph_en.as_default():
+        state_value = encoder_model.predict(vec)
+    y_decoder = np.zeros((1, 1, n_char))  # decoderの出力を格納する配列
+    y_decoder[0][0][char_indices['\t']] = 1  # decoderの最初の入力はタブ。one-hot表現にする。
+
+    respond_sentence = ""  # 返答の文字列
+    while True:
+        global graph
+        with graph_de.as_default():
+            y, h = decoder_model.predict([y_decoder, state_value])
+        p_power = y[0][0] ** beta  # 確率分布の調整
+        next_index = np.random.choice(len(p_power), p=p_power/np.sum(p_power)) 
+        next_char = indices_char[next_index]  # 次の文字
+        
+        if (next_char == "\n" or len(respond_sentence) >= max_length_x):
+            break  # 次の文字が改行のとき、もしくは最大文字数を超えたときは終了
+            
+        respond_sentence += next_char
+        y_decoder = np.zeros((1, 1, n_char))  # 次の時刻の入力
+        y_decoder[0][0][next_index] = 1
+
+        state_value = h  # 次の時刻の状態
+
+    return respond_sentence
 ###################################
 
 # MessageEvent
@@ -216,7 +248,7 @@ def handle_message(event):
                     line_bot_api.reply_message(
                             event.reply_token,
                             [
-                                    TextSendMessage(text=mono.respond(message, max_length_x, n_char, char_indices, indices_char, beta=5))
+                                    TextSendMessage(text=respond(message, max_length_x, n_char, char_indices, indices_char, beta=5))
                             ]
                     )
             elif (event.message.text == "終了"):
