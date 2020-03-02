c3b9483b0504c206b71a6a7351279babc4e7b993
安江洸希 <yasuekouki@ankou.local>
2020-03-03 06:47:50 +0900

    add new file

--- main.py
+++ main.py
@@ -326,7 +326,7 @@ def handle_message(event):
                     )
     if activity == 'make_story':
         if event.type == "message":
-            if (event.message.text != "終了"):
+            if (event.message.text != "終了") or (event.message.text == "めでたしめでたし"):
                 message = event.message.text #入力文
                 if mono.is_invalid(message, chars_list):
                     line_bot_api.reply_message(
