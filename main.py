6cf9f12f4d4a9e3208c008a07272b22488b0d336
安江洸希 <yasuekouki@ankou.local>
2020-03-03 01:57:13 +0900

    add new file

--- main.py
+++ main.py
@@ -193,10 +193,30 @@ def handle_message(event):
                     )
                 elif (event.message.text != answer):
                         qui.change_db("1","flag")
+                        n=int(qui.get_db()[2])
+                        send_message = title[n]
+                        #正常に検索結果が返った場合
+                        try:
+                            wikipedia_page = wikipedia.page(send_message)
+                            # wikipedia.page()の処理で、ページ情報が取得できれば、以下のようにタイトル、リンク、サマリーが取得できる。
+                            wikipedia_title = wikipedia_page.title
+                            wikipedia_url = wikipedia_page.url
+                            wikipedia_summary = wikipedia.summary(send_message)
+                            reply_message = '【' + wikipedia_title + '】\n' + wikipedia_summary + '\n\n' + '【詳しくはこちら】\n' + wikipedia_url
+                        # ページが見つからなかった場合
+                        except wikipedia.exceptions.PageError:
+                            reply_message = '【' + send_message + '】\nについての情報は見つかりませんでした。'
+                        # 曖昧さ回避にひっかかった場合
+                        except wikipedia.exceptions.DisambiguationError as e:
+                            disambiguation_list = e.options
+                            reply_message = '複数の候補が返ってきました。以下の候補から、お探しの用語に近いものを再入力してください。\n\n'
+                            for word in disambiguation_list:
+                                reply_message += '・' + word + '\n'
                         line_bot_api.reply_message(
                            event.reply_token,
                            [
                                 TextSendMessage(text="負正解です。\n正解は"+answer+"です"),
+                                TextSendMessage(text=reply_message),
                                 TextSendMessage(text="もう一問やりますか？\n【はい/いいえ】"),
                             ]
                         )
